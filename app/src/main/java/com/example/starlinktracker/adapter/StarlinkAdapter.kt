449287060f1905475508d36ffb2e4d6bf9cee9d3
wiggy <wiggy90210@gmail.com>
2020-11-16 13:35:33 +0100

    StarlinkAdapter.kt

--- app/src/main/java/com/example/starlinktracker/adapter/StarlinkAdapter.kt
+++ app/src/main/java/com/example/starlinktracker/adapter/StarlinkAdapter.kt
@@ -8,22 +8,29 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.starlinktracker.R
 import com.example.starlinktracker.retrofit.Results
+import com.example.starlinktracker.retrofit.SateliteResults
+import kotlinx.android.synthetic.main.item_starlink.view.*
 
 class StarlinkAdapter : RecyclerView.Adapter<StarlinkAdapter.StarlinkViewHolder>() {
 
     inner class StarlinkViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    val differList = AsyncListDiffer(this, listDifferCallback)
-
-    private val listDifferCallback = object : DiffUtil.ItemCallback<Results>() {
-        override fun areItemsTheSame(oldItem: Results, newItem: Results): Boolean {
-            //no results yet
+    private val listDifferCallback = object : DiffUtil.ItemCallback<SateliteResults>() {
+        override fun areItemsTheSame(oldItem: SateliteResults, newItem: SateliteResults): Boolean {
+            return oldItem.sateliteId == newItem.sateliteId
         }
 
-        override fun areContentsTheSame(oldItem: Results, newItem: Results): Boolean {
+        override fun areContentsTheSame(oldItem: SateliteResults, newItem: SateliteResults): Boolean {
+            val id = oldItem.sateliteId == newItem.sateliteId
+            val name = oldItem.sateliteName == newItem.sateliteName
+            val latitude = oldItem.latitude == newItem.longitude
+            val longitude = oldItem.longitude == newItem.longitude
 
+            return (id && name && latitude && longitude)
         }
     }
+    
+    val differList = AsyncListDiffer(this, listDifferCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StarlinkViewHolder {
         return StarlinkViewHolder(
@@ -37,11 +44,15 @@ class StarlinkAdapter : RecyclerView.Adapter<StarlinkAdapter.StarlinkViewHolder>
 
     override fun onBindViewHolder(holder: StarlinkViewHolder, position: Int) {
         val starlinkObject = differList.currentList[position]
+        holder.itemView.apply {
+            starlink_id.text = starlinkObject.sateliteId.toString()
+            starlink_name.text = starlinkObject.sateliteName
+            starlink_latitude.text = starlinkObject.latitude.toString()
+            starlink_longitude.text = starlinkObject.longitude.toString()
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = differList.currentList.size
 
 
 }
